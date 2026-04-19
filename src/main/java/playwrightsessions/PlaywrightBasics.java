3c91026ab790b044cfd7365d942c6c6b573363b0
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-04-19 23:42:11 +0530

    Downloaded Binary files

--- src/main/java/playwrightsessions/PlaywrightBasics.java
+++ src/main/java/playwrightsessions/PlaywrightBasics.java
@@ -1,6 +1,7 @@
 package playwrightsessions;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
 public class PlaywrightBasics {
@@ -8,8 +9,11 @@ public class PlaywrightBasics {
 	public static void main(String[] args) {
 
 		Playwright playwright = Playwright.create();
-
 		Browser browser = playwright.chromium().launch();
+		Page page = browser.newPage();
+		
+		System.out.println("Program completed");
+		
 	}
 
 }
