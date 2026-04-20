a7aa7b203a7e9196ca7efc698bc14ae33f06b469
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-04-20 22:48:39 +0530

    Ran in chromium browser.

--- src/main/java/playwrightsessions/PlaywrightBasics.java
+++ src/main/java/playwrightsessions/PlaywrightBasics.java
@@ -1,13 +1,31 @@
 package playwrightsessions;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.LoadState;
 
 public class PlaywrightBasics {
 
 	public static void main(String[] args) {
+		Playwright playwright = Playwright.create();
+		Browser browser =playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		Page page = browser.newPage();
 		
+		page.navigate("https://www.amazon.com");
+
+		// wait for title to be non-empty
+		page.waitForFunction("() => document.title.length > 0");
+
+		System.out.println("Page title is: " + page.title());
+		
+		String currentUrl = page.url();
+		System.out.println("URL of page is: "+currentUrl);
+		
+		page.close();
+		browser.close();
+		playwright.close();
 	}
 
 }
