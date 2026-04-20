e56cb0fbd9f234d1e17a3acc7c1bd119cf5e42f6
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-04-20 22:28:38 +0530

    Rewriting the first class

--- src/main/java/playwrightsessions/PlaywrightBasics.java
+++ src/main/java/playwrightsessions/PlaywrightBasics.java
@@ -7,12 +7,6 @@ import com.microsoft.playwright.Playwright;
 public class PlaywrightBasics {
 
 	public static void main(String[] args) {
-
-		Playwright playwright = Playwright.create();
-		Browser browser = playwright.chromium().launch();
-		Page page = browser.newPage();
-		
-		System.out.println("Program completed");
 		
 	}
 
