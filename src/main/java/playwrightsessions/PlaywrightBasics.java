8d8d8eea4c8f7bc88c63f0cceeb4e79af58c5116
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-04-23 21:24:59 +0530

    Practicing the basic class

--- src/main/java/playwrightsessions/PlaywrightBasics.java
+++ src/main/java/playwrightsessions/PlaywrightBasics.java
@@ -9,23 +9,7 @@ import com.microsoft.playwright.options.LoadState;
 public class PlaywrightBasics {
 
 	public static void main(String[] args) {
-		Playwright playwright = Playwright.create();
-		Browser browser =playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
-		Page page = browser.newPage();
 		
-		page.navigate("https://www.amazon.com");
-
-		// wait for title to be non-empty
-		page.waitForFunction("() => document.title.length > 0");
-
-		System.out.println("Page title is: " + page.title());
-		
-		String currentUrl = page.url();
-		System.out.println("URL of page is: "+currentUrl);
-		
-		page.close();
-		browser.close();
-		playwright.close();
 	}
 
 }
