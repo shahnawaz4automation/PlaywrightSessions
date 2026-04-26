4db110d665caae3d459e17ee7d343d0409b3faa2
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-04-26 21:10:34 +0530

    Wrote the Example program

---
+++ src/main/java/playwrightsessions/Example.java
@@ -0,0 +1,38 @@
+package playwrightsessions;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import java.util.*;
+
+public class Example {
+  public static void main(String[] args) {
+    try (Playwright playwright = Playwright.create()) {
+      Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+        .setHeadless(false));
+      BrowserContext context = browser.newContext();
+      Page page = context.newPage();
+      page.navigate("https://academy.naveenautomationlabs.com/");
+      page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Login")).click();
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Name")).click();
+      assertThat(page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Name"))).isVisible();
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Name")).click();
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Name")).fill("naveenautomation");
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Name")).press("ControlOrMeta+a");
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Name")).press("ControlOrMeta+c");
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Email address")).click();
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Email address")).fill("naveenautomation");
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Password")).click();
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Password")).fill("naveenautomation");
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Email address")).click();
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Email address")).fill("naveenautomation@gmail.com");
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.BUTTON, new FrameLocator.GetByRoleOptions().setName("India: +")).click();
+      page.locator("#microfe-popup-login").contentFrame().getByText("+91").click();
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Enter your number")).click();
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.TEXTBOX, new FrameLocator.GetByRoleOptions().setName("Enter your number")).fill("+91 81211-62925");
+      page.locator("#microfe-popup-login").contentFrame().getByRole(AriaRole.BUTTON, new FrameLocator.GetByRoleOptions().setName("Next")).click();
+      page.locator("#microfe-popup-login").contentFrame().getByText("Must contain atleast 1").click();
+      assertThat(page.locator("#microfe-popup-login").contentFrame().locator("form")).containsText("Must contain atleast 1 uppercase, 1 lowercase and 1 numeric characters. Minimum 8 characters.");
+    }
+  }
+}
--- src/main/java/playwrightsessions/PlaywrightBasics.java
+++ src/main/java/playwrightsessions/PlaywrightBasics.java
@@ -11,6 +11,22 @@ public class PlaywrightBasics {
 	public static void main(String[] args) {
 		Playwright playwright = Playwright.create();
 		Browser browser = playwright.chromium().launch();
+		Page page = browser.newPage();
+		
+		page.navigate("https://www.amazon.com");
+		
+		String title = page.title();
+		System.out.println("Page title is: " + title);
+		
+		String url = page.url();
+		System.out.println("URL is: "+url);
+		
+		String title1 = page.title();
+		System.out.println("Page title is: " + title1);
+		
+		page.close();
+		browser.close();
+		playwright.close();
 	}
 
 }
