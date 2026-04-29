7370786dae2b84e494c05ed70da3384ef9421fe6
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-04-29 21:54:54 +0530

    Updated

--- src/main/java/playwrightsessions/BrowserContextConcept.java
+++ src/main/java/playwrightsessions/BrowserContextConcept.java
@@ -9,16 +9,16 @@ import com.microsoft.playwright.Playwright;
 public class BrowserContextConcept {
 
 	public static void main(String[] args) {
-		
+
 		Playwright playwright = Playwright.create();
-		Browser browser =  playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-		
+		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
 		BrowserContext brcx1 = browser.newContext();
 		Page p1 = brcx1.newPage();
 		p1.navigate("https://omayo.blogspot.com/");
 		p1.fill("#ta1", "automation by sam");
 		System.out.println(p1.title());
-		
+
 		BrowserContext brcx2 = browser.newContext();
 		Page p2 = brcx2.newPage();
 		p2.navigate("https://omayo.blogspot.com/");
