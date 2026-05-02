17ee4bfc04caf264bbb546c3a3f660ce0df46d9b
Mohammed Shahnawaz <shah4corp@gmail.com>
2026-05-02 23:13:21 +0530

    Update

--- src/main/java/playwrightsessions/LocatorConcept.java
+++ src/main/java/playwrightsessions/LocatorConcept.java
@@ -7,17 +7,17 @@ public class LocatorConcept {
 	public static void main(String[] args) {
 		Playwright playwright = Playwright.create();
 		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-		
+
 		BrowserContext browserContext = browser.newContext();
 		Page page = browserContext.newPage();
 		page.navigate("https://academy.naveenautomationlabs.com/");
-		
+
 		Locator contactSales = page.locator("text=Login").first();
 		System.out.println(contactSales.count());
-		
+
 		contactSales.hover();
 		contactSales.click();
-		
+
 	}
 
 }
